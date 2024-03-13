660c6d37140fd0b082ddd287c44e316c6775253f
anshulkumarx7 <anshulkumarx7@gmail.com>
2024-03-13 16:00:59 +0530

    added models

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const express=require('express');
 const app=express();
 
 mongoose
