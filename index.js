4241adcaea38176ad1ab6f612bfb6a8382f4979d
anshulkumarx7 <anshulkumarx7@gmail.com>
2024-03-13 15:19:19 +0530

    db connection setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app=express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => console.log("DB connection successfull"))
+.catch((err) => console.log(err))
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Listening at port 5000");
+})
